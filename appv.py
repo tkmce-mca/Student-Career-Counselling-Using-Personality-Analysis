9e269ad14a92c1526e79e35792237a8f72890cce
shilpasaju <48026561+shilpasaju@users.noreply.github.com>
2019-11-17 08:32:44 -0800

    Add files via upload

---
+++ appv.py
@@ -0,0 +1,35 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('modelv.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction_text = model.predict(final_features)
+
+    return render_template('home.html', prediction_text=prediction_text)
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
